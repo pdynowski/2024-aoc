ff361d6b856011e23e52217e5bfdb7c23af347dd
Paul Dynowski <paul.dynowski@gmail.com>
2024-12-13 02:14:23 -0600

    Better, matrix-based solution for Day 13

--- 13/code.rb
+++ 13/code.rb
@@ -1,4 +1,5 @@
 require_relative '../utils/input_parser'
+require 'matrix'
 require 'pry'
 
 parser = ->(line) do
@@ -17,31 +18,35 @@ create_games = parsed_input.each_slice(4) do |group|
   game = {}
   game['a'] = { 'x' => group[0][1][1].to_i, 'y' => group[0][2][1].to_i }
   game['b'] = { 'x' => group[1][1][1].to_i, 'y' => group[1][2][1].to_i }
-  game['prize'] = { 'x' => group[2][1][1].to_i, 'y' => group[2][2][1].to_i }
+  game['prize'] = { 'x' => 10000000000000 + group[2][1][1].to_i, 'y' => 10000000000000 + group[2][2][1].to_i }
   games << game
 end
 
 
 def winning_pairs(game)
-  winning_pairs = []
-  (1..100).to_a.each do |a|
-    (1..100).to_a.each do |b|
-      if (game['a']['x'] * a + game['b']['x'] * b == game['prize']['x']) && (game['a']['y'] * a + game['b']['y'] * b == game['prize']['y'])
-        winning_pairs << [a,b]
-      end
-    end
-  end
-  winning_pairs
+  equations = [ [game['a']['x'], game['b']['x']], [game['a']['y'], game['b']['y']] ]
+  results = [ game['prize']['x'], game['prize']['y'] ]
+  matrix = Matrix.rows(equations)
+  res = Matrix.column_vector(results)
+  solutions = matrix.lup_decomposition.solve(res).to_a
+  a = solutions.first.first.denominator == 1 ? solutions.first.first.numerator : nil
+  b = solutions.last.first.denominator == 1 ? solutions.last.first.numerator : nil
+  [a, b].compact
 end
+
 
 winning_pairs = games.map do |game|
   winning_pairs(game)
 end
 
-cost = winning_pairs.reduce(0) do |sum, game|
-  sum + game.reduce(0) do |sum_g, winners|
-    sum_g + 3*winners[0] + winners[1]
+cost = winning_pairs.reduce(0) do |sum, pair|
+  if pair.empty? || (pair[0].nil? || pair[1].nil?)
+    sum
+  else
+    sum + 3*pair[0] + pair[1]
   end
 end
 
 p cost
+
+
