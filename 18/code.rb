b189eefea905adfa303e7e4b2c78a7d32162048e
Paul Dynowski <paul.dynowski@gmail.com>
2024-12-18 03:43:20 -0600

    Day 18 full solution

--- 18/code.rb
+++ 18/code.rb
@@ -1,7 +1,64 @@
 require_relative '../utils/input_parser'
+require_relative '../utils/mapping'
 require 'pry'
 
 parser = ->(line) do
+  arr = line.strip.split(',').map(&:to_i)
+  arr.length > 1 ? Complex(arr[0], arr[1]) : arr.first
 end
 
-parsed_input = InputParser.parse_input(test_state: true, parser: parser)
+parsed_input = InputParser.parse_input(test_state: false, parser: parser)
+
+bytes_to_use = parsed_input.shift
+grid_size = parsed_input.shift
+
+blocked_bytes = parsed_input.slice(0, bytes_to_use)
+
+map_input = []
+
+(0..grid_size).each do |y|
+  row = []
+  (0..grid_size).each do |x|
+    blocked_bytes.include?(Complex(x,y)) ? row << '#' : row << '.'
+  end
+  map_input << row
+end
+
+map = Mapping.new(map_input)
+
+# map.print_grid
+
+start = Complex(0,0)
+endpoint = Complex(grid_size, grid_size)
+
+p map.solve_maze(start, endpoint)
+
+btu_lower = bytes_to_use
+btu_upper = parsed_input.length - 1
+
+
+loop do
+  bytes_to_use = (btu_lower + btu_upper) / 2
+  puts btu_lower
+  puts bytes_to_use
+  puts btu_upper
+  puts
+  test_input = parsed_input.slice(0, bytes_to_use)
+  map_input = []
+  (0..grid_size).each do |y|
+    row = []
+      (0..grid_size).each do |x|
+        test_input.include?(Complex(x,y)) ? row << '#' : row << '.'
+      end
+      map_input << row
+  end
+  map = Mapping.new(map_input)
+
+  if map.solve_maze(start, endpoint)
+    btu_lower = bytes_to_use
+  else
+    btu_upper = bytes_to_use
+  end
+  break if btu_lower == btu_upper - 1
+end
+p parsed_input.slice(0, btu_upper).last
--- utils/mapping.rb
+++ utils/mapping.rb
@@ -2,10 +2,26 @@ class Mapping
 
   DIRS = [0-1i, 1+0i, 0+1i, -1+0i]
 
-  attr_reader :grid
+  attr_reader :grid, :walls
 
   def initialize(grid)
     @grid = grid
+    @walls = find_walls
+  end
+
+  def find_walls
+    wall_locs = []
+    grid.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        wall_locs << Complex(x, y) if cell == '#'
+      end
+    end
+    wall_locs
+  end
+
+  def add_wall(location)
+    walls << location
+    grid[location.imag][location.real] = '#'
   end
 
   def value(point)
@@ -22,6 +38,50 @@ class Mapping
 
   def in_y_range?(y)
     y.between?(0, grid.length - 1)
+  end
+
+  def print_grid
+    grid.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        print cell
+      end
+      puts
+    end
+    puts
+  end
+
+  def composite_key(location, facing)
+    "#{location.real},#{location.imag},#{DIRS.find_index(facing)}"
+  end
+
+  def solve_maze(start, endpoint, facing=Complex(1,0))
+    score = 0
+    visitation_queue = [[start, facing, 0]]
+    visited_locations = Set.new
+
+    while visitation_queue.length > 0 do
+      visitation_queue.sort! do |l1, l2| 
+        l1[2] <=> l2[2]
+      end
+
+      location, facing, score = visitation_queue.shift
+      key = composite_key(location, facing)
+
+      return score if location == endpoint
+      next if visited_locations.include?(key)
+
+      visited_locations.add(key)
+
+      new_location = location + facing
+
+      if !walls.include?(new_location) && on_grid?(new_location)
+        visitation_queue << [new_location, facing, score + 1]
+      end
+
+      visitation_queue << [location, facing*Complex(0,-1), score]
+      visitation_queue << [location, facing*Complex(0,1), score]
+    end 
+    return false
   end
 
 end
